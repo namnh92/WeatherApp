a4aa3e5e6cb333bb5da6121c054605c28ce94d38
Max Nguyen <namnh.code4fun@gmail.com>
2025-12-27 11:57:38 +0700

    Usecase3,4,5: Add UserDefault to Store City Recent

--- WeatherApp/Domain/Entities/City.swift
+++ WeatherApp/Domain/Entities/City.swift
@@ -5,7 +5,8 @@
 //  Created by Max Nguyen on 26/12/25.
 //
 
-protocol ICity: Equatable, Hashable {
+protocol ICity: Codable, Equatable, Hashable {
+    var id: String { get }
     var name: String { get }
     var country: String { get }
     var region: String? { get }
@@ -14,9 +15,19 @@ protocol ICity: Equatable, Hashable {
 }
 
 struct City: ICity {
+    let id: String
     let name: String
     let country: String
     let region: String?
     let latitude: Double
     let longitude: Double
+    
+    init(name: String, country: String, region: String?, latitude: Double, longitude: Double) {
+        self.id = name + country + (region ?? "") + latitude.description + longitude.description
+        self.name = name
+        self.country = country
+        self.region = region
+        self.latitude = latitude
+        self.longitude = longitude
+    }
 }
